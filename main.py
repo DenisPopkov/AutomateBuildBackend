cedd521c5498005a8db223dd51ac9a63a3d855b0
DenisPopkov <popkov-den03@mail.ru>
2025-01-21 11:51:44 +0600

    add: update params to build_mac_signed

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import subprocess
 
 app = Flask(__name__)
 
-
-@app.route('/build_mac_signed', methods=['POST'])
-def build_mac_signed():
+@app.route('/build_mac', methods=['POST'])
+def build_mac():
     try:
-        script_path = "./build_mac_signed.sh"
+        branch_name = request.json.get('branchName')
+        sign = request.json.get('sign')
+
+        if not branch_name or sign is None:
+            return jsonify({"error": "Missing required parameters: branchName and sign"}), 400
+
+        if sign:
+            script_path = f"./build_mac_signed.sh"
+        else:
+            script_path = f"./build_mac_signed_no_sign.sh"
 
         subprocess.run(["sh", script_path], check=True)
 
-        return jsonify({"message": "macOS build and signing executed successfully!"}), 200
+        return jsonify({"message": f"macOS build for branch {branch_name} {'with' if sign else 'without'} signing executed successfully!"}), 200
+
     except subprocess.CalledProcessError as e:
         return jsonify({"error": str(e)}), 500
-
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
