9a7d5b228cb8998f2ea2842e73646340ad831068
DenisPopkov <popkov-den03@mail.ru>
2025-01-22 09:59:15 +0600

    add: android method

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, jsonify, request
 import subprocess
 
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
+
 
 @app.route('/build_mac', methods=['POST'])
 def build_mac():
@@ -19,12 +21,34 @@ def build_mac():
 
         subprocess.run(["sh", script_path], check=True)
 
-        return jsonify({"message": f"macOS build for branch {branch_name} {'with' if sign else 'without'} signing executed successfully!"}), 200
+        return jsonify({
+            "message": f"macOS build for branch {branch_name} {'with' if sign else 'without'} signing executed successfully!"}), 200
 
     except subprocess.CalledProcessError as e:
         return jsonify({"error": str(e)}), 500
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+@app.route('/build_android', methods=['POST'])
+def build_android():
+    try:
+        branch_name = request.json.get('branchName')
+
+        if not branch_name:
+            return jsonify({"error": "Missing required parameter: branchName"}), 400
+
+        script_path = f"./build_android.sh"
+
+        subprocess.run(["sh", script_path], check=True)
+
+        return jsonify({"message": f"Android build for branch {branch_name} executed successfully!"}), 200
+
+    except subprocess.CalledProcessError as e:
+        return jsonify({"error": str(e)}), 500
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)
