49e56fd912c4df3ac0e2c20768e86b8ddfcaea6b
DenisPopkov <popkov-den03@mail.ru>
2025-01-26 18:55:58 +0600

    add: stop process option

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import os
 import subprocess
-
 from flask import Flask, jsonify, request
 
 from BuildItem import BuildItem
-from utils import extract_version
+from utils import extract_version, get_pid, kill_process
 
 app = Flask(__name__)
+
+
+@app.route('/stop_process', methods=['POST'])
+def stop_process():
+    try:
+        process_name = request.json.get("processName")
+
+        if not process_name:
+            return jsonify({"error": "Missing required parameter: processName"}), 400
+
+        pids = get_pid(process_name)
+
+        for pid in pids:
+            result = kill_process(pid)
+            if result != 0:
+                return jsonify({"message": f"Failed to stop process with PID {pid}"}), 500
+
+        return jsonify({"message": f"Successfully stopped all processes for {process_name}"}), 200
+
+    except ValueError as e:
+        return jsonify({"message": str(e)}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route('/build_mac', methods=['POST'])
@@ -14,20 +36,12 @@ def build_mac():
     try:
         branch_name = request.json.get('branchName')
         sign = request.json.get('sign')
-
         if not branch_name or sign is None:
             return jsonify({"error": "Missing required parameters: branchName and sign"}), 400
-
-        if sign:
-            script_path = f"./build_mac_signed.sh"
-        else:
-            script_path = f"./build_mac_signed_no_sign.sh"
-
+        script_path = f"./build_mac_signed.sh" if sign else f"./build_mac_signed_no_sign.sh"
         subprocess.run(["sh", script_path, branch_name], check=True)
-
         return jsonify({
             "message": f"macOS build for branch {branch_name} {'with' if sign else 'without'} signing executed successfully!"}), 200
-
     except subprocess.CalledProcessError as e:
         return jsonify({"error": str(e)}), 500
     except Exception as e:
@@ -38,16 +52,11 @@ def build_mac():
 def build_android():
     try:
         branch_name = request.json.get('branchName')
-
         if not branch_name:
             return jsonify({"error": "Missing required parameter: branchName"}), 400
-
         script_path = "./build_android.sh"
-
         subprocess.run(["sh", script_path, branch_name], check=True)
-
         return jsonify({"message": f"Android build for branch {branch_name} executed successfully!"}), 200
-
     except subprocess.CalledProcessError as e:
         return jsonify({"error": str(e)}), 500
     except Exception as e:
@@ -58,16 +67,11 @@ def build_android():
 def build_ios():
     try:
         branch_name = request.json.get('branchName')
-
         if not branch_name:
             return jsonify({"error": "Missing required parameter: branchName"}), 400
-
         script_path = "./build_ios.sh"
-
         subprocess.run(["sh", script_path, branch_name], check=True)
-
         return jsonify({"message": f"iOS build for branch {branch_name} executed successfully!"}), 200
-
     except subprocess.CalledProcessError as e:
         return jsonify({"error": str(e)}), 500
     except Exception as e:
@@ -78,10 +82,8 @@ def build_ios():
 def get_builds():
     try:
         builds_folder = "/Users/denispopkov/Desktop/builds"
-
         if not os.path.exists(builds_folder):
             return jsonify({"error": f"The 'builds' folder does not exist in {builds_folder}"}), 404
-
         build_files = [
             {
                 "file_name": f,
@@ -90,12 +92,9 @@ def get_builds():
             for f in os.listdir(builds_folder)
             if os.path.isfile(os.path.join(builds_folder, f))
         ]
-
         build_files = sorted(build_files, key=lambda x: x["creation_time"], reverse=True)
-
         build_items = []
         build_id = 1
-
         for file in build_files:
             file_name = file["file_name"]
             if file_name.endswith(".pkg"):
@@ -110,9 +109,7 @@ def get_builds():
                 version = extract_version(file_name)
                 build_items.append(BuildItem(id=build_id, version=version, platform_name="Windows"))
                 build_id += 1
-
         return jsonify([item.to_dict() for item in build_items])
-
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -121,41 +118,25 @@ def get_builds():
 def send_build():
     try:
         build_id = request.json.get('buildId')
-
         if not build_id:
             return jsonify({"error": "Missing required parameter: buildId"}), 400
-
         builds_folder = "/Users/denispopkov/Desktop/builds"
-
         if not os.path.exists(builds_folder):
             return jsonify({"error": f"The 'builds' folder does not exist in {builds_folder}"}), 404
-
-        # Filter files to only include .apk, .pkg, and .msi
         valid_extensions = ['.apk', '.pkg', '.msi']
         build_files = [
             f for f in os.listdir(builds_folder)
             if os.path.isfile(os.path.join(builds_folder, f)) and f.lower().endswith(tuple(valid_extensions))
         ]
-
-        # Sort the build files by modification time (most recent first)
         build_files = sorted(build_files, key=lambda f: os.path.getmtime(os.path.join(builds_folder, f)), reverse=True)
-
         if build_id <= 0 or build_id > len(build_files):
             return jsonify(
                 {"error": f"Invalid buildId: {build_id}. It should be between 1 and {len(build_files)}."}), 400
-
-        # Get the file for the selected build_id
         selected_build_file = build_files[build_id - 1]
         build_file_path = os.path.join(builds_folder, selected_build_file)
-
         script_path = "./send.sh"
-
         subprocess.run(["sh", script_path, build_file_path], check=True)
-
-        return jsonify({
-            "message": f"Build {build_id} ({selected_build_file}) sent successfully!"
-        }), 200
-
+        return jsonify({"message": f"Build {build_id} ({selected_build_file}) sent successfully!"}), 200
     except subprocess.CalledProcessError as e:
         return jsonify({"error": f"Failed to run send.sh: {str(e)}"}), 500
     except Exception as e:
@@ -166,30 +147,19 @@ def send_build():
 def get_remote_branches():
     try:
         repo_path = "/Users/denispopkov/AndroidStudioProjects/SA_Neuro_Multiplatform"
-
         if not os.path.exists(repo_path):
             return jsonify({"error": f"The repository does not exist at {repo_path}"}), 404
-
         subprocess.run(["git", "-C", repo_path, "fetch", "--all"], check=True)
-
         result = subprocess.run(
             ["git", "-C", repo_path, "branch", "-r"],
             check=True,
             stdout=subprocess.PIPE,
             text=True
         )
-
         branches = result.stdout.strip().split("\n")
         branch_names = [branch.strip().replace("origin/", "") for branch in branches if
                         branch.strip().startswith("origin/")]
-
         return jsonify({"branches": branch_names}), 200
-
-    except subprocess.CalledProcessError as e:
-        return jsonify({"error": f"Failed to fetch branches: {str(e)}"}), 500
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
     except subprocess.CalledProcessError as e:
         return jsonify({"error": f"Failed to fetch branches: {str(e)}"}), 500
     except Exception as e:
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import re
+import subprocess
+import subprocess as sub
 
 
 def extract_version(file_name):
@@ -10,3 +12,34 @@ def extract_version(file_name):
         build = match.group(3)  # The build number inside parentheses, e.g., "321"
         return f"{version} ({build})"
     return "unknown"
+
+
+def get_pid(process_name: str) -> list[int]:
+    cmd = ['pgrep', '-f', process_name]
+
+    with sub.Popen(cmd, stdout=sub.PIPE) as proc:
+        result = proc.communicate()[0]
+
+    result = result.decode().strip()
+
+    if not result:
+        raise ValueError(f"Process name {process_name} not found.")
+
+    return list(map(int, result.splitlines()))
+
+
+def kill_process(pid: int | str) -> bool:
+    cmd = ['kill', str(pid)]
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as proc:
+        err = proc.communicate()[1]
+    return bool(err)
+
+
+def stop_process_by_name(process_name: str) -> str:
+    try:
+        pid = get_pid(process_name)
+        if kill_process(pid):
+            return f"Failed to stop process: {process_name}"
+        return f"Process {process_name} stopped successfully"
+    except ValueError as e:
+        return str(e)
