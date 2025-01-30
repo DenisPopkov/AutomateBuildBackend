9440296268852b791a40e68f7ffec25f4508f170
Denis Popkov <popkov-den03@mail.ru>
2025-01-30 11:08:41 +0600

    add: update script

--- build_ios.sh
+++ build_ios.sh
@@ -100,13 +100,8 @@ cp "$SWIFT_FILE_SOURCE" "$SWIFT_TARGET_FILE"
 
 cd "$IOS_APP_PATH" || exit
 
-# Sync Gradle (if this is an Android project)
-echo "Running Gradle sync..."
-cd "/Users/denispopkov/AndroidStudioProjects/SA_Neuro_Multiplatform" || exit
-./gradlew sync
-
 # Run Fastlane with fallback
-if fastlane testflight_upload; then
+if export DISABLE_SWIFTLINT=true && fastlane testflight_upload; then
   git add .
   git commit -m "iOS version bump to $NEW_VERSION"
   git push
