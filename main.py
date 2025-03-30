7d3d5cb2be9b46a1a6a6566b7502316292af7ffc
DenisPopkov <popkov-den03@mail.ru>
2025-03-30 09:14:48 +0600

    add: update script

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ app = Flask(__name__)
 @app.route('/build_mac', methods=['POST'])
 def build_mac():
     try:
-        secret_file_path = "/Users/denispopkov/Desktop/secret.txt"
-        secret_config = read_secret_file(secret_file_path)
-
         data = request.json
         branch_name = data.get('branchName')
         use_dev_analytics = data.get('isUseDevAnalytics', True)
@@ -28,7 +25,7 @@ def build_mac():
             process = subprocess.Popen(
                 ["sh", script_path, branch_name, use_dev_analytics_flag],
                 stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
                 text=True
             )
 
@@ -37,16 +34,10 @@ def build_mac():
                 sys.stdout.flush()
                 log.write(line)
 
-            for line in process.stderr:
-                sys.stderr.write(line)
-                sys.stderr.flush()
-                log.write(line)
-
             process.wait()
 
-            if process.returncode != 0:
-                post_error_message(branch_name, secret_config)
-                raise subprocess.CalledProcessError(process.returncode, script_path, output=line)
+        if process.returncode != 0:
+            raise subprocess.CalledProcessError(process.returncode, script_path)
 
         return jsonify({
             "message": f"macOS build for branch {branch_name} signing executed successfully!"
@@ -55,15 +46,12 @@ def build_mac():
     except subprocess.CalledProcessError:
         return jsonify({"error": "Build script failed. Check Slack for full logs."}), 500
     except Exception as e:
-        return jsonify({"error": f"An unexpected error occurred: {str(e)}"}), 500
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route('/rebuild_dsp', methods=['POST'])
 def rebuild_dsp():
     try:
-        secret_file_path = "/Users/denispopkov/Desktop/secret.txt"
-        secret_config = read_secret_file(secret_file_path)
-
         data = request.json
         branch_name = data.get('branchName')
 
@@ -77,7 +65,7 @@ def rebuild_dsp():
             process = subprocess.Popen(
                 ["sh", script_path, branch_name],
                 stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
                 text=True
             )
 
@@ -86,16 +74,10 @@ def rebuild_dsp():
                 sys.stdout.flush()
                 log.write(line)
 
-            for line in process.stderr:
-                sys.stderr.write(line)
-                sys.stderr.flush()
-                log.write(line)
-
             process.wait()
 
-            if process.returncode != 0:
-                post_error_message(branch_name, secret_config)
-                raise subprocess.CalledProcessError(process.returncode, script_path)
+        if process.returncode != 0:
+            raise subprocess.CalledProcessError(process.returncode, script_path)
 
         return jsonify({
             "message": f"macOS build for branch {branch_name} signing executed successfully!"
@@ -104,15 +86,12 @@ def rebuild_dsp():
     except subprocess.CalledProcessError:
         return jsonify({"error": "Build script failed. Check Slack for full logs."}), 500
     except Exception as e:
-        return jsonify({"error": f"An unexpected error occurred: {str(e)}"}), 500
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route('/rebuild_android_dsp', methods=['POST'])
 def rebuild_android_dsp():
     try:
-        secret_file_path = "/Users/denispopkov/Desktop/secret.txt"
-        secret_config = read_secret_file(secret_file_path)
-
         data = request.json
         branch_name = data.get('branchName')
 
@@ -126,7 +105,7 @@ def rebuild_android_dsp():
             process = subprocess.Popen(
                 ["sh", script_path, branch_name],
                 stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
                 text=True
             )
 
@@ -135,17 +114,10 @@ def rebuild_android_dsp():
                 sys.stdout.flush()
                 log.write(line)
 
-            for line in process.stderr:
-                sys.stderr.write(line)
-                sys.stderr.flush()
-                log.write(line)
-
             process.wait()
 
-            if process.returncode != 0:
-                print("error occurred")
-                post_error_message(branch_name, secret_config)
-                raise subprocess.CalledProcessError(process.returncode, script_path)
+        if process.returncode != 0:
+            raise subprocess.CalledProcessError(process.returncode, script_path)
 
         return jsonify({
             "message": f"macOS build for branch {branch_name} signing executed successfully!"
@@ -154,28 +126,31 @@ def rebuild_android_dsp():
     except subprocess.CalledProcessError:
         return jsonify({"error": "Build script failed. Check Slack for full logs."}), 500
     except Exception as e:
-        return jsonify({"error": f"An unexpected error occurred: {str(e)}"}), 500
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route('/build_win', methods=['POST'])
 def build_win():
     try:
-        secret_file_path = "/Users/denispopkov/Desktop/secret.txt"
-        secret_config = read_secret_file(secret_file_path)
-
+        print("Received request to /build_win")
         data = request.json
         branch_name = data.get('branchName')
-        log_file = "/tmp/build_error_log.txt"
         use_dev_analytics = data.get('isUseDevAnalytics', True)
+
+        print(
+            f"Parsed data: branchName={branch_name}, isUseDevAnalytics={use_dev_analytics}")
 
         if not branch_name:
             print("Error: Missing required parameter: branchName")
             return jsonify({"error": "Missing required parameter: branchName"}), 400
 
+        # Define the script path and flags
         script_path = ".\\build_win.ps1"
         use_dev_analytics_flag = "true" if use_dev_analytics else "false"
 
+        print("Changing working directory...")
         os.chdir("C:\\Users\\BlackBricks\\PycharmProjects\\AutomateBuildBackend")
+        print("Current working directory:", os.getcwd())
 
         command = [
             "powershell", "-ExecutionPolicy", "Bypass", "-File", script_path,
@@ -183,46 +158,29 @@ def build_win():
             "-USE_DEV_ANALYTICS", use_dev_analytics_flag
         ]
 
-        with open(log_file, "w") as log:
-            process = subprocess.Popen(
-                command,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                text=True
-            )
+        print(f"Executing command: {' '.join(command)}")
 
-            for line in process.stdout:
-                sys.stdout.write(line)
-                sys.stdout.flush()
-                log.write(line)
+        result = subprocess.run(command, check=True, capture_output=True, text=True)
 
-            for line in process.stderr:
-                sys.stderr.write(line)
-                sys.stderr.flush()
-                log.write(line)
-
-            process.wait()
-
-        if process.returncode != 0:
-            post_error_message(branch_name, secret_config)
-            raise subprocess.CalledProcessError(process.returncode, script_path)
+        print(f"Script output: {result.stdout}")
+        if result.stderr:
+            print(f"Script error output: {result.stderr}")
 
         return jsonify({
             "message": f"Windows build for branch {branch_name} executed successfully!"
         }), 200
 
     except subprocess.CalledProcessError as e:
+        print(f"Subprocess failed: {e}")
         return jsonify({"error": f"Build failed: {e}"}), 500
     except Exception as e:
+        print(f"Unexpected error: {e}")
         return jsonify({"error": f"Unexpected error: {str(e)}"}), 500
 
 
 @app.route('/build_android', methods=['POST'])
 def build_android():
     try:
-        secret_file_path = "/Users/denispopkov/Desktop/secret.txt"
-        secret_config = read_secret_file(secret_file_path)
-
         data = request.json
         branch_name = data.get('branchName')
         use_dev_analytics = data.get('isUseDevAnalytics', True)
@@ -232,8 +190,7 @@ def build_android():
 
         script_path = "./build_android.sh"
         log_file = "/tmp/build_error_log.txt"
-
-        is_bundle_to_build_flag = "false" if use_dev_analytics else "true"
+        is_bundle_to_build_flag = "true" if not use_dev_analytics else "false"
         use_dev_analytics_flag = "true" if use_dev_analytics else "false"
 
         with open(log_file, "w"):
@@ -243,7 +200,7 @@ def build_android():
             process = subprocess.Popen(
                 ["sh", script_path, branch_name, is_bundle_to_build_flag, use_dev_analytics_flag],
                 stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
                 text=True
             )
 
@@ -252,15 +209,9 @@ def build_android():
                 sys.stdout.flush()
                 log.write(line)
 
-            for line in process.stderr:
-                sys.stderr.write(line)
-                sys.stderr.flush()
-                log.write(line)
-
             process.wait()
 
         if process.returncode != 0:
-            post_error_message(branch_name, secret_config)
             raise subprocess.CalledProcessError(process.returncode, script_path)
 
         return jsonify({
@@ -270,15 +221,12 @@ def build_android():
     except subprocess.CalledProcessError:
         return jsonify({"error": "Build script failed. Check Slack for full logs."}), 500
     except Exception as e:
-        return jsonify({"error": f"Unexpected error: {str(e)}"}), 500
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route('/build_ios', methods=['POST'])
 def build_ios():
     try:
-        secret_file_path = "/Users/denispopkov/Desktop/secret.txt"
-        secret_config = read_secret_file(secret_file_path)
-
         data = request.json
         branch_name = data.get('branchName')
         use_dev_analytics = data.get('isUseDevAnalytics', True)
@@ -290,35 +238,25 @@ def build_ios():
         log_file = "/tmp/build_error_log.txt"
         use_dev_analytics_flag = "true" if use_dev_analytics else "false"
 
-        # Clear any previous log file
         with open(log_file, "w"):
             pass
 
-        # Open log file and execute the script
         with open(log_file, "w") as log:
             process = subprocess.Popen(
                 ["sh", script_path, branch_name, use_dev_analytics_flag],
                 stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,  # Explicitly capture stderr as well
+                stderr=subprocess.STDOUT,
                 text=True
             )
 
-            # Capture and log both stdout and stderr
             for line in process.stdout:
                 sys.stdout.write(line)
                 sys.stdout.flush()
                 log.write(line)
 
-            for line in process.stderr:
-                sys.stderr.write(line)
-                sys.stderr.flush()
-                log.write(line)
-
             process.wait()
 
-        # If the process fails (non-zero exit code), send error message
         if process.returncode != 0:
-            post_error_message(branch_name, secret_config)
             raise subprocess.CalledProcessError(process.returncode, script_path)
 
         return jsonify({"message": f"iOS build for branch {branch_name} executed successfully!"}), 200
@@ -326,7 +264,7 @@ def build_ios():
     except subprocess.CalledProcessError:
         return jsonify({"error": "Build script failed. Check Slack for full logs."}), 500
     except Exception as e:
-        return jsonify({"error": f"Unexpected error: {str(e)}"}), 500
+        return jsonify({"error": str(e)}), 500
 
 
 @app.route('/remote_branches', methods=['GET'])
@@ -360,36 +298,6 @@ def get_remote_branches():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
-def read_secret_file(secret_file_path):
-    config = {}
-    try:
-        with open(secret_file_path, 'r') as secret_file:
-            for line in secret_file:
-                key, value = line.strip().split('=', 1)
-                config[key.strip()] = value.strip()
-                print(f"value: ${value.strip()}")
-    except Exception as e:
-        print(f"Error reading secret file: {e}")
-    return config
-
-
-def post_error_message(branch_name, secret_config):
-    try:
-        SLACK_BOT_TOKEN = secret_config.get("SLACK_BOT_TOKEN")
-        SLACK_CHANNEL = secret_config.get("SLACK_CHANNEL")
-
-        message = f":x: Failed to update DSP library on `{branch_name}`"
-
-        subprocess.run(
-            ["/bin/bash", "/Users/denispopkov/PycharmProjects/AutomateBuildBackend/slack_upload.sh",
-             "post_message", SLACK_BOT_TOKEN, SLACK_CHANNEL, message],
-            check=True
-        )
-    except subprocess.CalledProcessError as e:
-        print(f"Error sending error message to Slack: {e}")
-    except Exception as e:
-        print(f"Error in post_error_message: {e}")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001)
