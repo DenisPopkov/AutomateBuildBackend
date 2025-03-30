e4e7c0917d8ce116ecdcefc8010bc70677396fd3
Denis Popkov <popkov-den03@mail.ru>
2025-03-30 12:28:46 +0600

    add: update scripts

--- rebuild_android_dsp.sh
+++ rebuild_android_dsp.sh
@@ -31,6 +31,9 @@ done < "$SECRET_FILE"
 
 BRANCH_NAME=$1
 
+message=":hammer_and_wrench: Start Android DSP library update on \`$BRANCH_NAME\`"
+post_message "${SLACK_BOT_TOKEN}" "${SLACK_CHANNEL}" "$message"
+
 open -a "Android Studio"
 
 cd "$PROJECT_DIR" || { echo "Project directory not found!"; exit 1; }
