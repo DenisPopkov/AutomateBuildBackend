865e37796f51253342ccafb7d91653fe9062962e
Denis Popkov <popkov-den03@mail.ru>
2025-04-05 21:52:32 +0600

    add: update script

--- build_win.sh
+++ build_win.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-source "./slack_upload.sh"
-source "./utils.sh"
-
 BRANCH_NAME=$1
 
-git pull origin "$BRANCH_NAME" --no-rebase
+open -a "Android Studio"
+
+PROJECT_DIR="/Users/denispopkov/AndroidStudioProjects/SA_Neuro_Multiplatform"
+cd "$PROJECT_DIR" || { echo "Project directory not found!"; exit 1; }
+
+echo "Checking out branch: $BRANCH_NAME"
+git stash push -m "Pre-build stash"
+git fetch && git checkout "$BRANCH_NAME" && git pull origin "$BRANCH_NAME" --no-rebase
 git commit -m "build: windows"
 git push origin "$BRANCH_NAME"
--- rebuild_windows_dsp.sh
+++ rebuild_windows_dsp.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-source "./slack_upload.sh"
-source "./utils.sh"
-
 BRANCH_NAME=$1
 
-git pull origin "$BRANCH_NAME" --no-rebase
-git commit -m "build: rebuild win dsp"
-git push origin "$BRANCH_NAME"
+open -a "Android Studio"
+
+PROJECT_DIR="/Users/denispopkov/AndroidStudioProjects/SA_Neuro_Multiplatform"
+cd "$PROJECT_DIR" || { echo "Project directory not found!"; exit 1; }
+
+echo "Checking out branch: $BRANCH_NAME"
+git stash push -m "Pre-build stash"
+git fetch && git checkout "$BRANCH_NAME" && git pull origin "$BRANCH_NAME" --no-rebase
