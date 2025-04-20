58cac3b14ac5d641a738f3ce2f8eb5df4bb496b6
Denis Popkov <popkov-den03@mail.ru>
2025-04-20 08:50:00 +0600

    add: update script

--- build_mac_signed.sh
+++ build_mac_signed.sh
@@ -3,8 +3,11 @@
 source "/Users/denispopkov/PycharmProjects/AutomateBuildBackend/slack_upload.sh"
 source "/Users/denispopkov/PycharmProjects/AutomateBuildBackend/utils.sh"
 
+PROJECT_DIR="/Users/denispopkov/AndroidStudioProjects/SA_Neuro_Multiplatform"
 SECRET_FILE="/Users/denispopkov/Desktop/secret.txt"
 BUILD_TOOL="/Users/denispopkov/AndroidStudioProjects/SA_Neuro_release/Neuro_desktop.pkgproj"
+DYLIB_PATH="$PROJECT_DIR/shared/src/commonMain/resources/MR/files/libkeychainbridge.dylib"
+SIGNED_PKG_PATH="$PROJECT_DIR/build/Neuro_desktopS.pkg"
 BUILD_PATH="$PROJECT_DIR/desktopApp/build"
 ERROR_LOG_FILE="/tmp/build_error_log.txt"
 
@@ -47,7 +50,6 @@ message=":hammer_and_wrench: MacOS build started on \`$BRANCH_NAME\` with $analy
 first_ts=$(post_message "${SLACK_BOT_TOKEN}" "${SLACK_CHANNEL}" "$message")
 echo "first_ts=$first_ts"
 
-PROJECT_DIR="/Users/denispopkov/AndroidStudioProjects/SA_Neuro_Multiplatform"
 cd "$PROJECT_DIR" || { echo "Project directory not found!"; exit 1; }
 
 echo "Checking out branch: $BRANCH_NAME"
@@ -181,9 +183,18 @@ done
 sleep 20
 
 ## Signing the .pkg
-SIGNED_PKG_PATH="/Users/denispopkov/AndroidStudioProjects/SA_Neuro_release/build/Neuro_desktopS.pkg"
 echo "Signing the .pkg file..."
 echo "$USER_PASSWORD" | sudo -S productsign --sign "Developer ID Installer: Source Audio LLC (Z2JAQC4DXV)" "$NOTARIZED_BUILD_PATH" "$SIGNED_PKG_PATH"
+
+# Sign the dylib
+echo "Signing the libkeychainbridge.dylib..."
+codesign --timestamp --options runtime --sign "Developer ID Application: Source Audio LLC (Z2JAQC4DXV)" "$DYLIB_PATH"
+
+# Path to the dylib
+
+# Sign the dylib
+echo "Signing the libkeychainbridge.dylib..."
+codesign --timestamp --options runtime --sign "Developer ID Application: Source Audio LLC (Z2JAQC4DXV)" "$DYLIB_PATH"
 
 sleep 20
 
