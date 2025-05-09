63fe196bdc7b3467c92626925ec40952e0f6cda9
Denis Popkov <popkov-den03@mail.ru>
2025-05-09 09:26:35 +0600

    fix: script

--- build_win.sh
+++ build_win.sh
@@ -11,10 +11,12 @@ PROJECT_DIR="/c/Users/BlackBricks/StudioProjects/SA_Neuro_Multiplatform"
 ERROR_LOG_FILE="${ERROR_LOG_FILE:-/tmp/build_error_log.txt}"
 ADVANCED_INSTALLER_CONFIG="/c/Users/BlackBricks/Applications/Neuro installer/installer_win/Neuro Desktop 2.aip"
 ADVANCED_INSTALLER_SETUP_FILES="/c/Users/BlackBricks/Applications/Neuro installer/installer_win/Neuro Desktop-SetupFiles"
+ADVANCED_INSTALLER_CLI="/c/Program Files (x86)/Caphyon/Advanced Installer 20.6/bin/x86/AdvancedInstaller.com"
 
 while IFS='=' read -r key value; do
   key=$(echo "$key" | xargs)
   value=$(echo "$value" | xargs)
+
   case "$key" in
     "SLACK_BOT_TOKEN") SLACK_BOT_TOKEN="$value" ;;
     "SLACK_CHANNEL") SLACK_CHANNEL="$value" ;;
@@ -28,33 +30,33 @@ post_error_message() {
 }
 
 cd "$PROJECT_DIR" || exit 1
-#git stash push -m "Pre-build stash"
-#git fetch --all
-#git checkout "$BRANCH_NAME"
-#git pull origin "$BRANCH_NAME" --no-rebase
+git stash push -m "Pre-build stash"
+git fetch --all
+git checkout "$BRANCH_NAME"
+git pull origin "$BRANCH_NAME" --no-rebase
 
 VERSION_CODE=$(grep '^desktop\.build\.number\s*=' gradle.properties | sed 's/.*=\s*\([0-9]*\)/\1/' | xargs)
 VERSION_NAME=$(grep '^desktop\.version\s*=' gradle.properties | sed 's/.*=\s*\([0-9]*\.[0-9]*\.[0-9]*\)/\1/' | xargs)
-#VERSION_CODE=$((VERSION_CODE + 1))
-#
-#sed -i "s/^desktop\.build\.number\s*=\s*[0-9]*$/desktop.build.number=$VERSION_CODE/" gradle.properties
-#git add gradle.properties
-#git commit -m "Windows version bump to $VERSION_CODE"
-#git push origin "$BRANCH_NAME"
+VERSION_CODE=$((VERSION_CODE + 1))
+
+sed -i "s/^desktop\.build\.number\s*=\s*[0-9]*$/desktop.build.number=$VERSION_CODE/" gradle.properties
+git add gradle.properties
+git commit -m "Windows version bump to $VERSION_CODE"
+git push origin "$BRANCH_NAME"
 
 analyticsMessage="prod"
 [ "$isUseDevAnalytics" == "true" ] && analyticsMessage="dev"
 
-#end_time=$(date -d "+15 minutes" +"%H:%M")
-#message=":hammer_and_wrench: Windows build started on \`$BRANCH_NAME\` with $analyticsMessage analytics. It will be ready approximately at $end_time"
-#first_ts=$(post_message "$SLACK_BOT_TOKEN" "$SLACK_CHANNEL" "$message")
-#
-#if [ "$isUseDevAnalytics" == "false" ]; then
-#  enable_prod_keys
-#  sleep 5
-#  powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('^(+o)')"
-#  sleep 50
-#fi
+end_time=$(date -d "+15 minutes" +"%H:%M")
+message=":hammer_and_wrench: Windows build started on \`$BRANCH_NAME\` with $analyticsMessage analytics. It will be ready approximately at $end_time"
+first_ts=$(post_message "$SLACK_BOT_TOKEN" "$SLACK_CHANNEL" "$message")
+
+if [ "$isUseDevAnalytics" == "false" ]; then
+  enable_prod_keys
+  sleep 5
+  powershell -command "Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait('^(+o)')"
+  sleep 50
+fi
 
 ./gradlew packageReleaseMsi || { post_error_message "$BRANCH_NAME"; exit 1; }
 
@@ -67,31 +69,25 @@ NEW_MSI_PATH="$DESKTOP_BUILD_PATH/Neuro_Desktop-${VERSION_NAME}-${VERSION_CODE}.
 mv "$MSI_FILE" "$NEW_MSI_PATH"
 
 EXTRACT_DIR="$ADVANCED_INSTALLER_SETUP_FILES/Neuro_Desktop-${VERSION_NAME}-${VERSION_CODE}"
-mkdir -p "$EXTRACT_DIR"
+C:/ProgramData/chocolatey/bin/lessmsi.exe x "$NEW_MSI_PATH" "$EXTRACT_DIR"
 
-LESSMSI_PATH_WIN=$(cygpath -w "$NEW_MSI_PATH")
-EXTRACT_DIR_WIN=$(cygpath -w "$EXTRACT_DIR")
+EXTRACTED_APP_PATH="$EXTRACT_DIR/SourceDir/ProgramFilesFolder/Source Audio/Neuro Desktop 3"
+rm -rf "$ADVANCED_INSTALLER_SETUP_FILES/app" "$ADVANCED_INSTALLER_SETUP_FILES/realtime"
+cp -r "$EXTRACTED_APP_PATH/app" "$ADVANCED_INSTALLER_SETUP_FILES/"
+cp -r "$EXTRACTED_APP_PATH/realtime" "$ADVANCED_INSTALLER_SETUP_FILES/"
 
-echo "Extracting MSI from: $LESSMSI_PATH_WIN"
-echo "To directory: $EXTRACT_DIR_WIN"
-
-powershell -Command "lessmsi x '$LESSMSI_PATH_WIN' '$EXTRACT_DIR_WIN'"
-
-EXTRACTED_APP_PATH="${EXTRACT_DIR}/SourceDir/ProgramFilesFolder/Source Audio/Neuro Desktop 3"
-rm -rf "${ADVANCED_INSTALLER_SETUP_FILES}/app" "${ADVANCED_INSTALLER_SETUP_FILES}/realtime"
-cp -r "${EXTRACTED_APP_PATH}/app" "${ADVANCED_INSTALLER_SETUP_FILES}/"
-cp -r "${EXTRACTED_APP_PATH}/realtime" "${ADVANCED_INSTALLER_SETUP_FILES}/"
-
-OLD_VERSION=$(grep 'Property Id="ProductVersion"' "$ADVANCED_INSTALLER_CONFIG" | sed -n 's/.*Value="\([^"]*\)".*/\1/p')
+OLD_VERSION=$(grep -oP 'Property Id="ProductVersion" Value="\K[^"]+' "$ADVANCED_INSTALLER_CONFIG")
 sed -i "s/Property Id=\"ProductVersion\" Value=\"$OLD_VERSION\"/Property Id=\"ProductVersion\" Value=\"$VERSION_NAME\"/" "$ADVANCED_INSTALLER_CONFIG"
 
-OLD_GENERATE_CODE=$(grep 'Property Id="GenerateCode"' "$ADVANCED_INSTALLER_CONFIG" | sed -n 's/.*Value="\([^"]*\)".*/\1/p')
-NEW_GENERATE_CODE=$((OLD_GENERATE_CODE + 1))
-sed -i "s/Property Id=\"GenerateCode\" Value=\"$OLD_GENERATE_CODE\"/Property Id=\"GenerateCode\" Value=\"$NEW_GENERATE_CODE\"/" "$ADVANCED_INSTALLER_CONFIG"
+GENERATE_CODE=$(grep -oP 'Property Id="GenerateCode" Value="\K[^"]+' "$ADVANCED_INSTALLER_CONFIG")
+NEXT_GENERATE_CODE=$((GENERATE_CODE + 1))
+sed -i "s/Property Id=\"GenerateCode\" Value=\"$GENERATE_CODE\"/Property Id=\"GenerateCode\" Value=\"$NEXT_GENERATE_CODE\"/" "$ADVANCED_INSTALLER_CONFIG"
+
+# Запуск AdvancedInstaller, используя конфигурацию проекта
+powershell -Command "Start-Process -Wait -NoNewWindow 'C:/Program Files (x86)/Caphyon/Advanced Installer 20.6/bin/x86/AdvancedInstaller.exe' -ArgumentList '/build', '$ADVANCED_INSTALLER_CONFIG'"
 
 SIGNED_MSI_PATH="$ADVANCED_INSTALLER_SETUP_FILES/Neuro_Desktop-${VERSION_NAME}-${VERSION_CODE}.msi"
-
-#/c/Program\ Files\ \(x86\)/Windows\ Kits/10/bin/10.0.20348.0/x86/signtool.exe sign /fd sha256 /tr http://ts.ssl.com /td sha256 /sha1 20fbd34014857033bcc6dabfae390411b22b0b1e "$SIGNED_MSI_PATH"
+# signtool sign /fd sha256 /tr http://ts.ssl.com /td sha256 /sha1 20fbd34014857033bcc6dabfae390411b22b0b1e "$SIGNED_MSI_PATH"
 
 execute_file_upload "$SLACK_BOT_TOKEN" "$SLACK_CHANNEL" ":white_check_mark: Windows build for \`$BRANCH_NAME\`" "upload" "$SIGNED_MSI_PATH"
 delete_message "$SLACK_BOT_TOKEN" "$SLACK_CHANNEL" "$first_ts"
