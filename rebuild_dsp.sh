80f92c07164750c9c8e321ac23320a23e6a12e74
Denis Popkov <popkov-den03@mail.ru>
2025-06-26 17:46:17 +0600

    add: dsp dev

--- rebuild_dsp.sh
+++ rebuild_dsp.sh
@@ -45,11 +45,9 @@ echo "isUseDevAnalytics param: $IS_USE_DEV_ANALYTICS"
 if [[ "$IS_USE_DEV_ANALYTICS" == "true" ]]; then
   HEROKU_PATH="$HEROKU_PROD"
   HEROKU_LIBRARY_PATH="$HEROKU_LIBRARY"
-  ENV_LABEL="prod"
 else
   HEROKU_PATH="$HEROKU_DEV"
   HEROKU_LIBRARY_PATH="$HEROKU_LIBRARY_DEV"
-  ENV_LABEL="dev"
 fi
 
 cd "$PROJECT_DIR" || { echo "Project directory not found!"; exit 1; }
